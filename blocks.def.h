8cbe1038c6573f897e298c59a7d533bd45561609
dominictoms <dominictoms97@gmail.com>
2021-07-18 04:44:05 +0100

    minor tweaks

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/	/*Command*/                                                          /*Interval*/  /*Signal*/
-    {"",        "hostname",                                                          0,            0},
-    {"",        "amixer sget Master | awk -F '[][]' '/%/ { print $2 }' | head -n1",  0,            1},
-    {"",        "echo $(cat /sys/class/power_supply/BAT0/capacity)%",                1,            0},
-    {"",        "echo $(free -m|awk 'NR==2{print$3}')M",                             1,            0},
-    {"",        "date +%A' '%B' '%d' '%H:%M:%S",                                     1,            0},
+     /*Icon*/ /*Command*/                                                                   /*Interval*/  /*Signal*/
+    {"",      "hostname",                                                                   0,            0},
+    {"",      "echo $(pactl list sinks | grep '^[[:space:]]Volume:' | awk '{print $5}')",  0,            1},
+    {"",      "echo $(cat /sys/class/power_supply/BAT0/capacity)%",                         1,            0},
+    {"",      "echo $(free -m|awk 'NR==2{print$3}')M",                                      1,            0},
+    {"",      "date +%A' '%B' '%d' '%H:%M:%S",                                              1,            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
