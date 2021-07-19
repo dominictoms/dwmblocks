53c1ba68fad59598053cccdffc101e32c9b04df5
dominictoms <dominictoms97@gmail.com>
2021-07-19 07:26:46 +0100

    added brightness

--- blocks.def.h
+++ blocks.def.h
@@ -2,7 +2,8 @@
 static const Block blocks[] = {
      /*Icon*/ /*Command*/                                                                   /*Interval*/  /*Signal*/
     {"",      "hostname",                                                                   0,            0},
-    {"",      "echo $(pactl list sinks | grep '^[[:space:]]Volume:' | awk '{print $5}')",  0,            1},
+    {"",      "echo $(pactl list sinks | grep '^[[:space:]]Volume:' | awk '{print $5}')",   0,            1},
+    {"",      "echo $(xbacklight -get | cut -d. -f1)%",                                     0,            2},
     {"",      "echo $(cat /sys/class/power_supply/BAT0/capacity)%",                         1,            0},
     {"",      "echo $(free -m|awk 'NR==2{print$3}')M",                                      1,            0},
     {"",      "date +%A' '%B' '%d' '%H:%M:%S",                                              1,            0},
