88c918131a72b08ebbedfac24f502f2005f9f412
dominictoms <dominictoms97@gmail.com>
2021-08-04 10:27:30 +0100

    improved shell compatibility

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,14 @@
 /* modify this file to change what commands output to your statusbar, and recompile using the make command */
 static const Block blocks[] = {
-	/*icon        command                                                                                                                int  sig*/
-	{ "\uf108 ",  "echo $(whoami)@$(hostname)",                                                                                          0,   0 },
-	{ "\uf1eb ",  "w=$(cat /proc/net/wireless | awk 'NR==3 {print $3}'); [[ -z $w ]] && echo offline || echo ${w//.}%",                  1,   0 },
-	{ "\uf58f ",  "[[ $(pactl list sinks | awk '/Mute/{print $2}') = 'no' ]] && pactl list sinks | awk '/Vol/{print $5}' || echo mute",  0,   1 },
-	{ "\uf042 ",  "echo $(xbacklight -get | awk -F . '{print $1}')%",                                                                    0,   2 },
-	{ "\uf240 ",  "b=/sys/class/power_supply/BAT0/capacity; [[ -f $b ]] && echo $(cat $b)% || echo none",                                1,   0 },
-	{ "\uf538 ",  "free -h | awk 'NR == 2 {print $3}'",                                                                                  1,   0 },
-	{ "\uf783 ",  "date '+%A %d %B'",                                                                                                    1,   0 },
-	{ "\uf017 ",  "date '+%H:%M:%S'",                                                                                                    1,   0 },
+	/*icon        command                                                                                                              int  sig*/
+	{ "\uf108 ",  "echo $(whoami)@$(hostname)",                                                                                        0,   0 },
+	{ "\uf1eb ",  "w=$(cat /proc/net/wireless | awk 'NR==3 {print $3}'); [ -z $w ] && echo offline || echo ${w}% | tr -d '.'",         1,   0 },
+	{ "\uf58f ",  "[ $(pactl list sinks | awk '/Mute/{print $2}') = 'no' ] && pactl list sinks | awk '/Vol/{print $5}' || echo mute",  0,   1 },
+	{ "\uf042 ",  "echo $(xbacklight -get | awk -F . '{print $1}')%",                                                                  0,   2 },
+	{ "\uf240 ",  "b=/sys/class/power_supply/BAT0/capacity; [ -f $b ] && echo $(cat $b)% || echo none",                                1,   0 },
+	{ "\uf538 ",  "free -h | awk 'NR == 2 {print $3}'",                                                                                1,   0 },
+	{ "\uf783 ",  "date '+%A %d %B'",                                                                                                  1,   0 },
+	{ "\uf017 ",  "date '+%H:%M:%S'",                                                                                                  1,   0 },
 };
 
 /* sets delimeter between status commands. NULL character ('\0') means no delimeter */
